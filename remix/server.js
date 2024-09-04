2afe879d3a1be72889a947086b4ca7721e1b1407
Coaspe <aspalt85@gmail.com>
2024-09-04 12:57:34 +0900

    Ready for deployment

--- remix/server.js
+++ remix/server.js
@@ -22,24 +22,18 @@ const app = express();
 
 app.use(compression());
 
-// http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
 app.disable("x-powered-by");
 
-// handle asset requests
 if (viteDevServer) {
   app.use(viteDevServer.middlewares);
 } else {
-  // Vite fingerprints its assets so we can cache forever.
   app.use("/assets", express.static("build/client/assets", { immutable: true, maxAge: "1y" }));
 }
 
-// Everything else (like favicon.ico) is cached for an hour. You may want to be
-// more aggressive with this caching.
 app.use(express.static("build/client", { maxAge: "1h" }));
 
 app.use(morgan("tiny"));
 
-// handle SSR requests
 app.all("*", remixHandler);
 
 const port = process.env.PORT || 3000;
