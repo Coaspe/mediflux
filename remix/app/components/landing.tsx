add7007c646f3b23bde61b4fa42e5e8fc27782d0
Coaspe <aspalt85@gmail.com>
2024-09-12 14:19:38 +0900

    refactor: Refactor login and register action

--- remix/app/components/landing.tsx
+++ remix/app/components/landing.tsx
@@ -14,8 +14,7 @@ export const LoginButton = ({ name, onClick }: { name: string; onClick: () => vo
   return (
     <button
       onClick={onClick}
-      className="animated-button bg-button font-work text-white font-semibold text-3xl py-4 px-10 rounded-xl shadow-lg transition-all delay-0 duration-200 ease-out [translate:0] hover:[translate:0_-2px]"
-    >
+      className="animated-button select-none bg-button font-work text-white font-semibold text-3xl py-4 px-10 rounded-xl shadow-lg transition-all delay-0 duration-200 ease-out [translate:0] hover:[translate:0_-2px]">
       {name}
     </button>
   );
@@ -136,14 +135,12 @@ export const LoginModal: React.FC<LoginModalProps> = ({ setIsModalOpen }) => {
       enterTo="opacity-100"
       leave="transition-opacity duration-300 ease-in-out"
       leaveFrom="opacity-100"
-      leaveTo="opacity-0 none"
-    >
+      leaveTo="opacity-0 none">
       <div ref={divRef} onTransitionEnd={onTransitionEnd} onClick={closeModal} className={`fixed bg-black bg-opacity-50 inset-0 flex items-center justify-center example-style`}>
         <div
           onClick={(event) => event.stopPropagation()}
           style={{ height: isLoginMode ? "400px" : "500px" }}
-          className={`flex flex-col items-center relative bg-white p-8 rounded-md w-[400px] transition-all`}
-        >
+          className={`flex flex-col items-center relative bg-white p-8 rounded-md w-[400px] transition-all`}>
           <Icon onClick={closeModal} className={"absolute right-0 top-0 pr-3 pt-3 cursor-pointer"} iconName={ICONS.CLOSE} />
           <span className="font-work font-semibold text-2xl">{isLoginMode ? "Login" : "Register"}</span>
           <div className="flex flex-col justify-evenly items-center h-full">
@@ -181,8 +178,7 @@ export const LoginModal: React.FC<LoginModalProps> = ({ setIsModalOpen }) => {
                         onChange={(event) => {
                           setRole(event.target.value as Role);
                         }}
-                        value={role}
-                      >
+                        value={role}>
                         <option value="doctor">의사</option>
                         <option value="nurse">간호사</option>
                         <option value="staff">스태프</option>
@@ -236,8 +232,7 @@ export const LoginModal: React.FC<LoginModalProps> = ({ setIsModalOpen }) => {
                 {!isLoading ? (
                   <button
                     className="bg-button hover:bg-blue-800 text-gray-200 font-noto font-bold py-2.5 px-4 rounded-3xl focus:outline-none focus:shadow-outline w-full transition-colors duration-200"
-                    type="submit"
-                  >
+                    type="submit">
                     확인
                   </button>
                 ) : (
--- remix/app/routes/_index.tsx
+++ remix/app/routes/_index.tsx
@@ -5,51 +5,44 @@ import { LoginButton, LoginModal } from "~/components/Landing";
 import { useState } from "react";
 import { badRequest, checkSameIdExists } from "~/utils/request.server";
 import { createUserSession, getUserSession, login, register } from "~/services/session.server";
-import { LoginResponse, User } from "~/types/type";
-import { ROLE, ServerUser } from "shared";
+import { LoginResponse } from "~/types/type";
+import { ServerUser } from "shared";
 import { json } from "@remix-run/react";
 import { redirect } from "@remix-run/node";
 import "../css/Animation.scss";
 import { convertServerUserToClientUser } from "~/utils/utils";
 import { DEFAULT_REDIRECT } from "~/constant";
 
-async function validateUserid(userId: string) {
-  if (userId.length <= 3) {
-    return "아이디는 4글자 이상이어야합니다.";
-  }
+// 유효성 검사 함수 분리
+async function validateUserId(userId: string): Promise<string | undefined> {
+  if (userId.length <= 3) return "아이디는 4글자 이상이어야 합니다.";
 
   try {
     const result = await checkSameIdExists(userId);
-    if (result.status === 200) {
-      return undefined;
-    } else {
-      return result.data.message;
-    }
+    return result.status === 200 ? undefined : result.data.message;
   } catch (error: any) {
-    return error.response?.data?.message ? error.response?.data?.message : "서버 오류";
+    return error.response?.data?.message || "서버 오류";
   }
 }
 
-function validatePassword(password: string) {
-  if (password.length < 6) {
-    return "비밀번호는 7자 이상이어야합니다.";
-  }
+function validatePassword(password: string): string | undefined {
+  return password.length < 6 ? "비밀번호는 7자 이상이어야 합니다." : undefined;
 }
-function validateConfirm(password: string, confirm: string) {
-  if (password !== confirm) {
-    return "비밀번호가 일치하지 않습니다.";
-  }
+
+function validateConfirmPassword(password: string, confirm: string): string | undefined {
+  return password !== confirm ? "비밀번호가 일치하지 않습니다." : undefined;
 }
-function validateUrl(url: string) {
+
+function validateUrl(url: string): string {
   return url;
 }
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  let sessionResult = await getUserSession(request);
-  if (sessionResult.status !== "active") {
-    return null;
+  const sessionResult = await getUserSession(request);
+  if (sessionResult.status === "active") {
+    return redirect("/dashboard/scheduling");
   }
-  return redirect("/dashboard/scheduling");
+  return null;
 }
 
 export const action: ActionFunction = async ({ request }: ActionFunctionArgs) => {
@@ -73,81 +66,70 @@ export const action: ActionFunction = async ({ request }: ActionFunctionArgs) =>
     });
   }
 
-  switch (requestType) {
-    case "login": {
-      let result = (await login({ userId, password })) as LoginResponse;
+  // 유효성 검사 분리
+  const fieldErrors = {
+    userId: await validateUserId(userId),
+    password: validatePassword(password),
+    confirm: requestType === "register" ? validateConfirmPassword(password, form.get("confirm") as string) : undefined,
+  };
 
-      if (result.status !== 200) {
-        const fieldErrors = {
-          userId: result.errorType === 1 ? result.message : undefined,
-          password: result.errorType === 2 ? result.message : undefined,
-        };
-
-        return badRequest({
-          fieldErrors,
-          formError: null,
-          serverError: result.errorType === 3,
-        });
-      }
-
-      const user = result.user as ServerUser;
-
-      const clientUser = { id: user.contact_id, loginId: user.login_id, role: ROLE.DOCTOR } as User;
-
-      return await createUserSession(clientUser, redirectTo, request);
-    }
-
-    case "register": {
-      const firstName = form.get("firstName");
-      const lastName = form.get("lastName");
-      const role = form.get("role");
-      const confirm = form.get("confirm");
-
-      if (typeof confirm !== "string" || typeof role !== "string" || typeof lastName !== "string" || typeof firstName !== "string") {
-        return badRequest({
-          fieldErrors: null,
-          fields: null,
-          formError: "Form not submitted correctly.",
-        });
-      }
-
-      const fieldErrors = {
-        userId: await validateUserid(userId),
-        password: validatePassword(password),
-        confirm: validateConfirm(password, confirm),
-      };
-
-      if (Object.values(fieldErrors).some(Boolean)) {
-        return badRequest({
-          fieldErrors,
-          formError: null,
-        });
-      }
-
-      let result = await register({ userId, password, role, firstName, lastName });
-
-      if (result.status === 200) {
-        const user = result.data.user as ServerUser;
-        const clientUser = convertServerUserToClientUser(user);
-
-        return await createUserSession(clientUser, redirectTo, request);
-      }
-
-      return badRequest({
-        fieldErrors: null,
-        formError: null,
-        serverError: true,
-      });
-    }
-
-    default: {
-      return badRequest({
-        fieldErrors: null,
-        formError: "Login type invalid",
-      });
-    }
+  if (Object.values(fieldErrors).some(Boolean) && requestType === "register") {
+    return badRequest({ fieldErrors, formError: null });
   }
+
+  return requestType === "login" ? handleLogin({ userId, password, redirectTo, request }) : handleRegister({ userId, password, form, redirectTo, request });
 };
+
+// 로그인 처리 함수
+async function handleLogin({ userId, password, redirectTo, request }: { userId: string; password: string; redirectTo: string; request: Request }) {
+  const result = (await login({ userId, password })) as LoginResponse;
+
+  if (result.status !== 200) {
+    const fieldErrors = {
+      userId: result.errorType === 1 ? result.message : undefined,
+      password: result.errorType === 2 ? result.message : undefined,
+    };
+
+    return badRequest({
+      fieldErrors,
+      formError: null,
+      serverError: result.errorType === 3,
+    });
+  }
+
+  const user = result.user as ServerUser;
+  const clientUser = convertServerUserToClientUser(user);
+
+  return createUserSession(clientUser, redirectTo, request);
+}
+
+// 회원가입 처리 함수
+async function handleRegister({ userId, password, form, redirectTo, request }: { userId: string; password: string; form: FormData; redirectTo: string; request: Request }) {
+  const firstName = form.get("firstName") as string;
+  const lastName = form.get("lastName") as string;
+  const role = form.get("role") as string;
+
+  if (!firstName || !lastName || !role) {
+    return badRequest({
+      fieldErrors: null,
+      formError: "Form not submitted correctly.",
+    });
+  }
+
+  const result = await register({ userId, password, role, firstName, lastName });
+
+  if (result.status === 200) {
+    const user = result.data.user as ServerUser;
+    const clientUser = convertServerUserToClientUser(user);
+    return createUserSession(clientUser, redirectTo, request);
+  }
+
+  return badRequest({
+    fieldErrors: null,
+    formError: null,
+    serverError: true,
+  });
+}
 
 export default function Index() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -157,17 +139,12 @@ export default function Index() {
       <div className="flex justify-center">
         <div className="rounded-lg animated-paper p-8 font-noto relative animated-title">
           <div className="overflow-hidden">
-            <h2 className="text-9xl font-bold font-playfair">Efficient care,</h2>
+            <h2 className="text-9xl font-bold font-playfair select-none">Efficient care,</h2>
           </div>
-          <div className=" overflow-hidden">
-            <h2 className="text-9xl font-bold font-playfair mb-10">Every time</h2>
+          <div className="overflow-hidden">
+            <h2 className="text-9xl font-bold font-playfair select-none mb-10">Every time</h2>
           </div>
-          <LoginButton
-            onClick={() => {
-              setIsModalOpen(true);
-            }}
-            name="Get started"
-          />
+          <LoginButton onClick={() => setIsModalOpen(true)} name="Get started" />
         </div>
       </div>
       {/* Login Modal */}
